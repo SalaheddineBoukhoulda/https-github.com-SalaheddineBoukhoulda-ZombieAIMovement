2dcf790ef5d5d0398a7a49b127162a1c75e9fc9a
RudeGames <fs_boukhoulda@esi.dz>
2018-08-14 23:01:15 +0100

    Added Moving forward
    Just simple movement toward the target

---
+++ Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public Transform goal;
+	public float speed = 0.5f;
+	public float rotSpeed = 0.4f;
+	public float accuracy = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+        this.transform.LookAt(lookAtGoal);
+		if(Vector3.Distance(this.transform.position,lookAtGoal)>accuracy)
+			this.transform.Translate(0,0,speed*Time.deltaTime);
+	}
+}
